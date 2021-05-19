79bfc4c1588d9bb72a779f8ca758f25e7a7cb2dc
John Napoline <50152447+johnnynaps@users.noreply.github.com>
2021-05-18 23:20:34 -0400

    Addressed review comment. Simulator only runs on specified profiles

--- quiztest/src/main/java/quiztest/main/inventory/DealershipSimulator.java
+++ quiztest/src/main/java/quiztest/main/inventory/DealershipSimulator.java
@@ -5,10 +5,12 @@ import java.sql.Timestamp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile({ "dev", "dev-windows", "debug" })
 public class DealershipSimulator {
 
     private Logger logger = LoggerFactory.getLogger(DealershipSimulator.class);
